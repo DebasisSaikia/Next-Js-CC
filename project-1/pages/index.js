6173f2ea6d8b0b931ddca2524dad33d21d98aa76
Debasis Saikia <debasishsaikia136@gmail.com>
2021-04-08 21:17:35 +0530

    nav-footer-added

--- project-1/pages/index.js
+++ project-1/pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div >
+      <Navbar />
       <h1>Next App</h1>
+      <Footer />
     </div>
   )
 }
