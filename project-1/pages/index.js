52d229fcd388f4cc3247e896f5af407075d2fa00
Debasis Saikia <debasishsaikia136@gmail.com>
2021-04-08 22:01:56 +0530

    styling added

--- project-1/pages/index.js
+++ project-1/pages/index.js
@@ -7,10 +7,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div >
-
-      <h1>Next App</h1>
+      <h1 className={styles.title} >Next App</h1>
+      <p className={styles.text}>Server side rendering and Static site generator</p>
       <Link href='/lists'>
-        See Listings
+        <a className={styles.btn}>See Listings</a>
       </Link>
 
     </div>
