c12d949540565b2f09a73268c134180a7b149adb
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-30 00:01:32 +0530

    contentful configured

--- next-cms/pages/index.js
+++ next-cms/pages/index.js
@@ -1,3 +1,13 @@
+import { createClient } from 'contentful'
+
+export async function getStaticProps() {
+
+  const credit = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  })
+}
+
 export default function Recipes() {
   return (
     <div className="recipe-list">
