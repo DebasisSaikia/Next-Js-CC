a2722def999201d045a7e11dd2e779fd714bb17d
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-30 00:10:59 +0530

    env set and data fetch

--- next-cms/pages/index.js
+++ next-cms/pages/index.js
@@ -6,9 +6,18 @@ export async function getStaticProps() {
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   })
+
+  const res = await credit.getEntries({ content_type: 'food' });
+
+  return {
+    props: {
+      foods: res.items
+    }
+  }
 }
 
-export default function Recipes() {
+export default function Recipes({ foods }) {
+  console.log(foods)
   return (
     <div className="recipe-list">
       Recipe List
