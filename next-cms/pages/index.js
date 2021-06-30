28bdf7e1eab0d1fdf7402ca14caebb78c96767e2
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-30 13:56:53 +0530

    styled jsx

--- next-cms/pages/index.js
+++ next-cms/pages/index.js
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful'
+import FoodCard from '../components/FoodCard';
 
 export async function getStaticProps() {
 
@@ -19,8 +20,20 @@ export async function getStaticProps() {
 export default function Recipes({ foods }) {
   console.log(foods)
   return (
-    <div className="recipe-list">
-      Recipe List
+    <div className="food-list">
+      {foods.map((food) => {
+        return (
+          <FoodCard key={food.sys.id} food={food} />
+        )
+      })}
+
+      <style jsx>{`
+    .food-list{
+      display:grid;
+      grid-template-columns:1fr 1fr;
+      grid-gap:20px 60px;
+    }
+          `}</style>
     </div>
   )
 }
