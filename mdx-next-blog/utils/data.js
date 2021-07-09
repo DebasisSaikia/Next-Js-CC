7f3b9e0c21d02e92d5d5901eedc6445f62f4aa58
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-09 16:38:11 +0530

    blog post and layout

---
+++ mdx-next-blog/utils/data.js
@@ -0,0 +1,17 @@
+export const blogPost = [
+    {
+        title: 'First Blog Post',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum, ad pariatur sequi eos ratione facere itaque commodi atque laboriosam! Numquam, nisi perspiciatis? Repudiandae necessitatibus corporis aliquid dicta ex obcaecati!'
+    },
+    {
+        title: 'Second Blog Post',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum, ad pariatur sequi eos ratione facere itaque commodi atque laboriosam! Numquam, nisi perspiciatis? Repudiandae necessitatibus corporis aliquid dicta ex obcaecati!'
+    },
+    {
+        title: 'Third Blog Post',
+        date: new Date(),
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum, ad pariatur sequi eos ratione facere itaque commodi atque laboriosam! Numquam, nisi perspiciatis? Repudiandae necessitatibus corporis aliquid dicta ex obcaecati!'
+    }
+]
