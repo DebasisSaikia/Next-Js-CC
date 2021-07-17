c70d7596780df83f77415c70ce519de02a106d82
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-17 13:50:02 +0530

    data saved to firebase

--- fb-clone/components/InputBox.js
+++ fb-clone/components/InputBox.js
@@ -1,21 +1,42 @@
+import { useRef } from 'react'
 import { useSession } from 'next-auth/client';
 import Image from 'next/image';
+import { db } from '../firebase'
+import firebase from 'firebase'
+import { CameraIcon } from '@heroicons/react/outline'
 
 const InputBox = () => {
 
     const [session] = useSession()
+    const inputRef = useRef(null)
 
     const sendPost = (e) => {
         e.preventDefault()
+
+        if (!inputRef.current.value) return;
+
+        db.collection('posts').add({
+            message: inputRef.current.value,
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        inputRef.current.value = ''
     }
+
     return (
         <div className="bg-white rounded-t-2xl p-2 shadow-md text-gray-500 mt-6">
             <div className='flex space-x-4 p-4 items-center'>
                 <Image alt={session.user.name} src={session.user.image} width={40} height={40} layout="fixed" className="rounded-full" />
                 <form className="flex flex-1">
-                    <input type="text" name="post" id="post" placeholder={`Post something ${session.user.name}`} className="outline-none h-12 bg-gray-200 flex-grow px-5 rounded-full" />
+                    <input type="text" name="post" id="post" ref={inputRef} placeholder={`Post something ${session.user.name}`} className="outline-none h-12 bg-gray-200 flex-grow px-5 rounded-full" />
                     <button type="submit" className="hidden" onClick={sendPost}>Submit</button>
                 </form>
+            </div>
+
+            <div>
+                <CameraIcon className="h-7 text-green-400 cursor-pointer" />
             </div>
         </div>
     )
--- fb-clone/firebase.js
+++ fb-clone/firebase.js
@@ -12,4 +12,7 @@ const firebaseConfig = {
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 const db = app.firestore()
+const storage = firebase.storage()
+
+export { db, storage }
 
