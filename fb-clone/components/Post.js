eceb39cd40d61e3e20c382c0e28e1215fb8bdafe
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-17 15:46:56 +0530

    data fetched from firebase

---
+++ fb-clone/components/Post.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+const Post = ({ name, message, email, timestamp, image, postImage }) => {
+
+    return (
+        <div className="flex flex-col">
+            <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-md">
+                <div className="flex items-center space-x-2">
+                    <img src={image} alt={name} height={40} width={40} className="rounded-full" />
+                    <div>
+                        <p className="font-semibold">{name}</p>
+                        <p className="text-xs text-gray-400">
+                            {new Date(timestamp?.toDate()).toLocaleString()}
+                        </p>
+                    </div>
+                </div>
+
+                <p className="pt-4">{message}</p>
+            </div>
+            {postImage && (
+                <div className="relative h-56 md:h-96 bg-white">
+                    <Image alt={`post by ${name}`} src={postImage} objectFit="contain" layout="fill" />
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export default Post
